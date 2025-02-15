5c8e2b900ca7f023c21d38afd23eadc668a8c2c4
SomasGH <somasundaram.pandian.in@gmail.com>
2025-02-15 06:14:18 -0500

    After error handling in server.py and emotion_detection.py

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -5,16 +5,24 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers = header)
-    emotions = json.loads(response.text)['emotionPredictions'][0]['emotion']
     emotions_dict = {}
     dominant = ""
-    for emotion in emotions:
-        emotions_dict[emotion] = emotions[emotion]
-        if dominant == "":
-            dominant = emotion
-            score = float(emotions[emotion])
-        elif float(emotions[emotion]) > score:
-            dominant = emotion
-            score = float(emotions[emotion])
-    emotions_dict['dominant_emotion'] = dominant
+    if response.status_code == 200:
+        emotions = json.loads(response.text)['emotionPredictions'][0]['emotion']
+        for emotion in emotions:
+            emotions_dict[emotion] = emotions[emotion]
+            if dominant == "":
+                dominant = emotion
+                score = float(emotions[emotion])
+            elif float(emotions[emotion]) > score:
+                dominant = emotion
+                score = float(emotions[emotion])
+            emotions_dict['dominant_emotion'] = dominant
+    elif response.status_code == 400:
+        emotions_dict['anger'] = None
+        emotions_dict['disgust'] = None
+        emotions_dict['fear'] = None
+        emotions_dict['joy'] = None
+        emotions_dict['sadness'] = None
+        emotions_dict['dominant_emotion'] = None
     return emotions_dict
--- server.py
+++ server.py
@@ -7,13 +7,14 @@ def emotion_detectors():
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
     rstmt = "For the given statement, the system response is "
+    if response['dominant_emotion'] == None:
+        return "Invalid text! Please try again"
     for i in response:
         if i != 'dominant_emotion' and i != 'sadness':
             rstmt = f"{rstmt} '{i}' : {response[i]},"
         elif i == 'sadness':
             rstmt = f"{rstmt} and '{i}' : {response[i]}"
-        else:
-            rstmt = f"{rstmt}. The dominant emotion is {response[i]}."    
+    rstmt = f"{rstmt}. The dominant emotion is {response['dominant_emotion']}."    
     return rstmt
 @app.route("/")
 def render_index_page():
